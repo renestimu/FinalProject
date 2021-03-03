86b8e3a7e1ea87f04e05f554639870cd9476cffa
Renestimu <renestimu@gmail.com>
2021-03-03 21:25:03 +0300

    03 ders oncesi

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -13,5 +13,14 @@ namespace Business.Constants
         public static string CategoryCountError = "Bir ürününün Kategori sayısı 10 geçemez";
         public static string ProductNameAlreadyExists = "Aynı isimde product ismi olamaz";
         public static string CategoryLimitExceded = "Kategori limiti aşıldı";
+        public static string AuthorizationDenied = "Reddedildi";
+
+
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
     }
 }
--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -29,7 +29,7 @@ namespace WebAPI.Controllers
             var result = _productService.GetAll();
             if (result.Success)
             {
-                return Ok(result);
+                return Ok(result.Data);
             }
             return BadRequest(result.Message);
         } 
